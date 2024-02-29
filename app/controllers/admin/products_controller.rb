a8a61f285c704473bb8dc104adbcc9baf069c085
Sophia <chiaki.katsuno@live.ca>
2024-02-28 20:56:06 -0800

    add http admin auth for admins products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,7 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: 
+  (ENV['HTTP_BASIC_AUTH_NAME'] || ''), password: 
+  (ENV['HTTP_BASIC_AUTH_PASSWORD'] || ''), except: :index
 
   def index
     @products = Product.order(id: :desc).all
