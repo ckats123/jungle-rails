7f6067bf35ae7c54966cdf454fc1a2765badfb85
Sophia <chiaki.katsuno@live.ca>
2024-02-29 12:57:51 -0800

    create about w. nav

---
+++ app/views/about/index.html.rb
@@ -0,0 +1,8 @@
+<h1>About Jungle</h1>
+<p>Immerse yourself in the captivating world of Jungle, 
+where we champion the life-altering influence of plants. 
+Our core mission revolves around enriching your life with the beauty and perks of nature. 
+We achieve this by presenting a diverse assortment of exquisite plants, 
+tailored to suit every taste and space. Whether you're an experienced plant aficionado or
+ just embarking on your journey to cultivate a green haven, Jungle stands as your premier 
+ destination for all things botanical.</p>
